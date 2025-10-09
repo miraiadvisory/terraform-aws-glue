6c6eca11d313edaa1260352d44eaf150bb8689fa
Miguel Garcia <2185056+miguelgherrera@users.noreply.github.com>
2025-10-09 11:41:02 +0200

    adding new behaviors for recrawl policy

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
 resource "aws_glue_crawler" "this" {
   database_name = var.database_name
   name          = var.name
-  role = var.role
+  role          = var.role
 
   s3_target {
     path = var.path
+  }
+
+  dynamic "recrawl_policy" {
+    for_each = var.recrawl_behavior == null ? [] : [var.recrawl_behavior]
+    content {
+      recrawl_behavior = recrawl_policy.value
+    }
   }
 
   provisioner "local-exec" {
--- variables.tf
+++ variables.tf
@@ -1,15 +1,28 @@
 variable "database_name" {
-    type = string
+  type = string
 }
+
 variable "name" {
-    type = string
+  type = string
 }
+
 variable "role" {
-    type = string
+  type = string
 }
+
 variable "path" {
-    type = string
+  type = string
 }
+
 variable "region" {
-    type = string
+  type = string
+}
+
+variable "recrawl_behavior" {
+  type    = string
+  default = null #According to provider documentation CRAWL_EVERYTHING is the default
+  validation {
+    condition     = var.recrawl_behavior == null || contains(["CRAWL_EVERYTHING", "CRAWL_NEW_FOLDERS_ONLY"], var.recrawl_behavior)
+    error_message = "recrawl_behavior it must be null, CRAWL_EVERYTHING o CRAWL_NEW_FOLDERS_ONLY. Check Mirai glue module for more info"
+  }
 }
